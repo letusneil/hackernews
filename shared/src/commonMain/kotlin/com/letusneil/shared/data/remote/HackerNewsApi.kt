429b992321b37aeaadc6bf3f5976a83ab6a0335b
neil <nbvinas@gmail.com>
2025-12-30 11:01:28 +0800

    feat: Update HackerNewsApi to use NewsResponse model

--- shared/src/commonMain/kotlin/com/letusneil/shared/data/remote/HackerNewsApi.kt
+++ shared/src/commonMain/kotlin/com/letusneil/shared/data/remote/HackerNewsApi.kt
@@ -1,12 +1,15 @@
 package com.letusneil.shared.data.remote
 
+import com.letusneil.shared.data.remote.model.NewsResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
 internal class HackerNewsApi(private val client: HttpClient) {
     private val baseUrl = "https://hn.algolia.com/api/v1"
 
-    suspend fun fetchTopStories(timestamp: Long, page: Int): AlgoliaNewsResponse {
+    suspend fun fetchTopStories(timestamp: Long, page: Int): NewsResponse {
         return client.get("$baseUrl/search") {
             parameter("tags", "story")
             parameter("numericFilters", "created_at_i>$timestamp,points>10")
@@ -15,7 +18,7 @@ internal class HackerNewsApi(private val client: HttpClient) {
         }.body()
     }
 
-    suspend fun fetchByCategory(categoryTag: String, page: Int): AlgoliaNewsResponse {
+    suspend fun fetchByCategory(categoryTag: String, page: Int): NewsResponse {
         return client.get("$baseUrl/search") {
             parameter("tags", categoryTag)
             parameter("hitsPerPage", 15)
