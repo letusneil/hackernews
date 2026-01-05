0e773f7f7ce0a6c6ade51fcc4df310544b910e1b
neil <nbvinas@gmail.com>
2026-01-05 20:27:32 +0800

    refactor(test): Make FakeHackerNewsApi response configurable

--- shared/src/commonTest/kotlin/com.letusneil.shared/data/remote/fakes/FakeHackerNewsApi.kt
+++ shared/src/commonTest/kotlin/com.letusneil.shared/data/remote/fakes/FakeHackerNewsApi.kt
@@ -9,24 +9,25 @@ class FakeHackerNewsApi : HackerNewsApi {
     var lastRequestedPage: Int? = null
     var shouldThrowError: Boolean = false
     var lastRequestedTimestamp: Long? = null
+    var responseHits: List<HitResponse> = listOf(
+        HitResponse(
+            id = "test_id",
+            title = "Test Title",
+            url = "",
+            author = "",
+            points = 21,
+            commentCount = 42,
+            createdAt = 33,
+            tags = listOf("top"),
+            storyText = "hello world"
+        )
+    )
 
     override suspend fun fetchTopStories(timestamp: Long, page: Int): NewsResponse {
         lastRequestedPage = page
         lastRequestedTimestamp = timestamp
         if (shouldThrowError) throw Exception("Fake API Error")
-        return NewsResponse(hits = listOf(
-            HitResponse(
-                id = "test_id",
-                title = "Test Title",
-                url = "",
-                author = "",
-                points = 21,
-                commentCount = 42,
-                createdAt = 33,
-                tags = listOf("top"),
-                storyText = "hello world"
-            )
-        ), nbPages = 12, page = 1, hitsPerPage = 12)
+        return NewsResponse(hits = responseHits, nbPages = 12, page = 1, hitsPerPage = 12)
     }
 
     override suspend fun fetchByCategory(
