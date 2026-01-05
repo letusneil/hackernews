99d7fd0a35a93f54094f90a975b1e9d2aeeede4b
neil <nbvinas@gmail.com>
2026-01-05 18:39:40 +0800

    feat: Integrate HomeScreen and remove boilerplate UI

--- app/src/main/java/com/letusneil/hackernews/MainActivity.kt
+++ app/src/main/java/com/letusneil/hackernews/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.letusneil.hackernews
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.letusneil.hackernews.ui.home.HomeScreen
 import com.letusneil.hackernews.ui.theme.HackernewsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HackernewsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen(
+                    onStoryClick = { story ->
+                        story.newsItem.url?.let { url ->
+                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                        }
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HackernewsTheme {
-        Greeting("Android")
     }
 }
