2d4bae44c21b8d23b58c99158ca294afd174b521
neil <nbvinas@gmail.com>
2026-01-05 20:43:25 +0800

    feat: Add Ktor client logging and Napier dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -74,7 +74,10 @@ kotlin {
                 // Networking
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.client.logging)
                 implementation(libs.ktor.serialization.json)
+
+                implementation(libs.napier)
             }
         }
 
