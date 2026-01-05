1e7813fbf0bd5dab2a737e0688fcf2d86c1277d3
neil <nbvinas@gmail.com>
2026-01-05 18:47:44 +0800

    feat: Add `isRefreshing` state to HomeUiState

--- shared/src/commonMain/kotlin/com/letusneil/shared/presentation/home/HomeUiState.kt
+++ shared/src/commonMain/kotlin/com/letusneil/shared/presentation/home/HomeUiState.kt
@@ -5,6 +5,7 @@ import com.letusneil.shared.domain.model.NewsFeedItem
 data class HomeUiState(
     val stories: List<NewsFeedItem> = emptyList(),
     val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
     val isLoadingMore: Boolean = false,
     val error: String? = null,
     val currentPage: Int = 0,
